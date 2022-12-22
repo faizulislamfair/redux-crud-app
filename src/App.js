6b2170d808df168137c1c45c486d29f894cdd98a
faizulislamfair <faizulislamfair@gmail.com>
2022-12-22 13:01:35 +0600

    add if value present

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
 
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
+  const [newUsername, setNewUsername] = useState("");
 
 
   return (
     <div className="App">
-      {" "}
+
+
       <div className="addUser mt-5">
         <input className='placeholder:italic placeholder:text-slate-400 shadow appearance-none border rounded py-3 pl-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2'
           type="text"
@@ -25,15 +27,26 @@ function App() {
             setName(event.target.value)
           }}
         />
+
+
+
         <input className='placeholder:italic placeholder:text-slate-400 shadow appearance-none border rounded py-3 pl-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2'
           type="text"
           placeholder='Username...'
+          name='sdf'
           onChange={(event) => {
             setUsername(event.target.value)
           }}
         />
-        <button onClick={() => { dispatch(addUser({ id: userList[userList.length - 1].id + 1, name: name, username: username })); }} className='btn bg-red-400 border-none text-white'>Add User</button>
+
+
+        <button onClick={() => { name && username && dispatch(addUser({ id: userList[userList.length - 1].id + 1, name: name, username: username })); }} className='btn bg-red-400 border-none text-white'>Add User</button>
       </div>
+
+
+
+
+
       <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-6 mt-12">
         {
           userList.map(user => {
@@ -43,16 +56,23 @@ function App() {
                   <h2 className="card-title">{user.username}</h2>
                   <p className='text-2xl'>{user.name}</p>
                   <div className="card-actions mt-2">
+
                     <input
                       type="text"
                       className='placeholder:italic placeholder:text-slate-400 shadow appearance-none border rounded py-1 pl-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                       placeholder="Update Username..."
                       onChange={(event) => {
-                        // setUsername(event.target.value);
+                        setNewUsername(event.target.value);
                       }}
                     />
+
                     <button className='btn bg-red-400 border-none text-white btn-sm'>Update Username</button>
+
+
+
                     <button onClick={() => { dispatch(deleteUser({ id: user.id })) }} className='btn bg-red-500 border-none btn-sm'>Delete User</button>
+
+
                   </div>
                 </div>
               </div>
@@ -60,6 +80,10 @@ function App() {
           })
         }
       </div>
+
+
+
+
     </div >
   );
 }
