f5836d2506808b28fe1f0e6fc25fd36c768ca4c0
faizulislamfair <faizulislamfair@gmail.com>
2022-12-22 12:39:48 +0600

    adding, displaying

---
+++ src/FakeData.js
@@ -0,0 +1,52 @@
+export const UsersData = [
+    {
+        id: 1,
+        name: "Saquib Jamil",
+        username: "Saquib",
+    },
+    {
+        id: 2,
+        name: "MD. Turag Islam",
+        username: "Turag",
+    },
+    {
+        id: 3,
+        name: "Mohammad Ali Kahaf",
+        username: "Kahaf",
+    },
+    {
+        id: 4,
+        name: "Roxy Sarkar",
+        username: "Roxy",
+    },
+    {
+        id: 5,
+        name: "Ahmed Taukif Reza Rafi",
+        username: "Rafi",
+    },
+    {
+        id: 6,
+        name: "MD. Rabbi",
+        username: "Rabbi",
+    },
+    {
+        id: 7,
+        name: "Abdullah Al Noman",
+        username: "Noman",
+    },
+    {
+        id: 8,
+        name: "Khalid Mohammod Shizan",
+        username: "Shizan",
+    },
+    {
+        id: 9,
+        name: "Sajid Shahriar",
+        username: "Sajid",
+    },
+    {
+        id: 10,
+        name: "MD. Mahmud Hasan",
+        username: "Roudro",
+    },
+];
